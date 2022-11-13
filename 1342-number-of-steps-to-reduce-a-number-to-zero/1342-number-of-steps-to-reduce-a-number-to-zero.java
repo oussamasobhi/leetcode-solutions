cccb83a6935893d0dca422daf9859a4f474bcb27
SOBHI Oussama <83254606+oussamasobhi@users.noreply.github.com>
2022-11-13 19:06:34 +0100

    Time: 1 ms (80.76%), Space: 40.4 MB (77.38%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,18 @@
+class Solution {
+  //  int count=0;
+    public int numberOfSteps(int num) {
+         if(num==0){
+             return 0;
+         }
+        if(num%2==0){
+          
+         return numberOfSteps(num/2)+1;
+       }
+        else {
+           
+          return  numberOfSteps(num-1)+1;
+        }
+    
+    
+    }
+}

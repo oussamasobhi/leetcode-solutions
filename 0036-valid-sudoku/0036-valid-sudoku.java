de7a6af48097b0e419c779673d4e451e81843055
SOBHI Oussama <83254606+oussamasobhi@users.noreply.github.com>
2022-11-25 19:59:48 +0100

    Time: 7 ms (59.78%), Space: 46.3 MB (59.64%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,54 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+         int startrow=0;
+         int startcol=0;
+         
+       int lenbox=0;
+        for(int p=0;p<board.length;p=p+3)
+        {
+            for(int q=0;q<board.length;q=q+3){     
+                HashSet<Character> set3=new HashSet<>();
+                for(int k=p;k<3+p;k++){
+                                          
+                    for(int m=q;m<3+q;m++){
+                        if(board[k][m]!='.'){
+                          set3.add(board[k][m]);
+                            lenbox++;
+                        }
+                    }
+                }
+                if(lenbox!=set3.size())
+                    return false;
+                lenbox=0;
+                }
+        }
+     
+         for(int i=0;i<board.length;i++){
+            HashSet<Character> set=new HashSet<>();
+            HashSet<Character> set2=new HashSet<>();
+             
+             int lenrow=0;
+             int lencol=0;
+             
+            
+             
+            for(int j=0;j<board.length;j++){
+               
+               if(board[i][j]!='.'){ 
+                 set.add(board[i][j]);
+                 lenrow++;
+               }
+                if(board[j][i]!='.'){
+                    set2.add(board[j][i]);
+                    lencol++;
+                }
+            }
+           
+            if(set2.size()!=lencol || set.size()!=lenrow)
+                return false;
+            else
+                continue;
+        }
+        return true;
+    }
+}

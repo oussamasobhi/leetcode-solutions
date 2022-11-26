b0c520470f0e4aea7a9123137c74d5333cbc284d
SOBHI Oussama <83254606+oussamasobhi@users.noreply.github.com>
2022-11-26 17:53:42 +0100

    Time: 0 ms (100.00%), Space: 79.5 MB (31.67%) - LeetHub

---
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findCenter(int[][] edges) {
+        if(edges[0][0]==edges[1][0]){
+            return edges[0][0];
+        }
+       else if(edges[0][1]==edges[1][1]){
+           return edges[0][1];
+       }
+        else if(edges[0][0]==edges[1][1]){
+            return edges[0][0];
+        }
+        else{
+            return edges[1][0];
+        }
+    }
+}

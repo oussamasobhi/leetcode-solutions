65e4388917fc6e2e1eb154155056329ca74096ea
SOBHI Oussama <83254606+oussamasobhi@users.noreply.github.com>
2023-08-21 01:21:19 +0100

    Time: 5 ms (77.59%), Space: 64.9 MB (6.62%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,32 +1,23 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-      Queue<Integer> before = new LinkedList<>();
-         Queue<Integer> after = new LinkedList<>();
-      List<Integer> pivots=new ArrayList<>();
-      for(int i=0;i<nums.length;i++){
-          if(nums[i]<pivot){
-              before.add(nums[i]);
+        int[] arr = new int[nums.length];
+        int j = 0;
+        for (int i : nums) {
+            if (i < pivot) {
+                arr[j++] = i;
+            }
+        }
+        for (int i : nums) {
+            if (i == pivot) {
+                arr[j++] = i;
+            }
+        }
+        for (int i : nums) {
+            if (i > pivot) {
+                arr[j++] = i;
+            }
+        }
 
-          }
-          else if(nums[i]>pivot){
-             after.add(nums[i]);
-          }
-          else{
-              pivots.add(nums[i]);                   
-          }
-      }
-      int j=0;
-      int[] arr=new int[nums.length];
-      for(Integer i:before){
-          arr[j++]=i.intValue();
-      }
-          for(Integer i:pivots){
-          arr[j++]=i.intValue();
-      }
-          for(Integer i:after){
-           arr[j++]=i.intValue();
-      }
-
-       return arr;
+        return arr;
       }
 }

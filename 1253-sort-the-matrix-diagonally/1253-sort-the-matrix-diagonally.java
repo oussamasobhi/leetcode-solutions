dce95e6666e3d9e32eb89f13e0f9a7f270a42a4d
SOBHI Oussama <83254606+oussamasobhi@users.noreply.github.com>
2023-08-26 16:48:16 +0100

    Time: 11 ms (23.26%), Space: 43.9 MB (71.82%) - LeetHub

--- 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.java
+++ 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-        // my approach is to not create another array and change the current matix
+        // my approach is to not create another matrix, ill just change the current one
+        // so i iterate through the diag, store it in a list, sort it and reinsert it 
         int colindex=0;
         int lineindex=mat.length-1;
         while(lineindex>=0 && colindex<mat[0].length){
         List<Integer> tmp=new ArrayList<>();
         for(int i=lineindex , j=colindex;i<mat.length && j<mat[0].length;i++,j++){
-           tmp.add(mat[i][j]);
-               
+           tmp.add(mat[i][j]);    
         }
         int k=0;
         Collections.sort(tmp);
+        // place the sorted diag back
         for(int i=lineindex , j=colindex;i<mat.length && j<mat[0].length;i++,j++){
            mat[i][j]=tmp.get(k++);
         }

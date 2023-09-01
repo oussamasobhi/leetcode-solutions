09b4406e5e85284827678e9c47cb1971e899c4b9
SOBHI Oussama <83254606+oussamasobhi@users.noreply.github.com>
2023-09-01 18:45:22 +0100

    Time: 1 ms (100%), Space: 43.8 MB (6.28%) - LeetHub

---
+++ 2844-sum-of-squares-of-special-elements/2844-sum-of-squares-of-special-elements.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(n%(i+1)==0)
+               sum+=Math.pow(nums[i],2);
+        }
+        return sum;
+    }
+}

fbc0523ec0be7ac99c339d6dbfaed0020d7073ed
SOBHI Oussama <83254606+oussamasobhi@users.noreply.github.com>
2023-09-20 13:39:51 +0100

    Time: 24 ms (70.97%), Space: 44.2 MB (60.05%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
@@ -1,15 +1,12 @@
 class Solution {
     public int partitionString(String s) {
         int ans=1;
-        ArrayList<Character> listy=new ArrayList<>();
+        int ref=0;
         for(int i=0;i<s.length();i++){
-            if(listy.contains(s.charAt(i))){
+            if(s.substring(ref,i).indexOf(s.charAt(i))!=-1){
                 ans++;
-                listy.clear();
-               // str=s.substring(i,i+1);
+                ref=i;
             }
-            listy.add(s.charAt(i));
-                //str+=s.substring(i,i+1);
         }
         return ans;
     }
